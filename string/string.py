98e364a7d631369c8d17f1cee9a6f9c8ebfc093d
muhammad2603 <fattahillahmuhammad48@gmail.com>
2024-10-21 21:48:55 +0700

    Type Data: String

---
+++ string/string.py
@@ -0,0 +1,10 @@
+# Belajar Type Data: String
+nama = "Muhammad Irwan Syaputra"
+kota = 'Jakarta'
+alamat = "Jalan Raya bla bla bla"
+
+nama_depan = "Muhammad"
+nama_belakang = "Syaputra"
+nama_lengkap = nama_depan + ' ' + nama_belakang
+
+print(nama_lengkap)
