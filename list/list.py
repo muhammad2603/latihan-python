3364919e7698b22a09fa832fe9ac043d611194ed
muhammad2603 <fattahillahmuhammad48@gmail.com>
2024-10-22 10:06:30 +0700

    Array: List

---
+++ list/list.py
@@ -0,0 +1,19 @@
+# Belajar List
+
+nama = ['Adi', 'budi', 'andi']
+nama.append('Jaka')
+
+print(nama)
+print(nama[0])
+print(nama[1])
+print(nama[2])
+print(nama[3])
+
+print(len(nama))
+
+nama.remove('budi')
+
+print(nama)
+print(nama[0])
+print(nama[1])
+print(nama[2])
