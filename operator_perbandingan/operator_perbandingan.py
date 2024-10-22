6a93c3ffde4518999f20190a280a0e71f878dc9d
muhammad2603 <fattahillahmuhammad48@gmail.com>
2024-10-22 09:51:14 +0700

    Operator Perbandingan

---
+++ operator_perbandingan/operator_perbandingan.py
@@ -0,0 +1,16 @@
+# Operator Perbandingan
+# 
+# > lebih dari
+# < kurang dari
+# >= lebih dari sama dengan
+# <= kurang dari sama dengan
+# == sama dengan
+# != tidak sama dengan
+# 
+
+print(10 <= 1)
+print(10 == 10)
+print(10 != 9)
+
+print("irwan" == "irwan")
+print("irwan" != "irwan")
