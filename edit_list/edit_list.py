dcfef045e24ff7b9f633fa43bc9e5494408a251a
muhammad2603 <fattahillahmuhammad48@gmail.com>
2024-10-22 10:10:18 +0700

    Edit List

---
+++ edit_list/edit_list.py
@@ -0,0 +1,16 @@
+# Membuat List
+nama = ['Adi', 'budi', 'andi']
+
+# Menambah Data ke List
+nama.append('jaka')
+
+# Mengakses List
+nama[0]
+
+# Menghapus Data dari List
+nama.remove('budi')
+
+# Mengubah Data di List
+print(nama)
+nama[0] = 'setiawan'
+print(nama)
