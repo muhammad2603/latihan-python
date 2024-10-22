4e16a9be3f0b28f680be52a80c23106948f323dd
muhammad2603 <fattahillahmuhammad48@gmail.com>
2024-10-22 10:13:58 +0700

    For Loop

---
+++ for_loop/for_loop.py
@@ -0,0 +1,10 @@
+# Belajar For Loop
+
+pelanggan = ['muhammad', 'budi', 'joko', 'andi']
+pelanggan.append('budiman')
+pelanggan.append('panji')
+
+for nama in pelanggan:
+    print("============================")
+    print(f"Nama Pelanggan: {nama}")
+    print("============================")
