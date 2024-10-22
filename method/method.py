b93daeba6b2c3c87aec9c70d211376dc3096deb3
muhammad2603 <fattahillahmuhammad48@gmail.com>
2024-10-22 11:49:46 +0700

    Method Function/Def

---
+++ method/method.py
@@ -0,0 +1,17 @@
+# Belajar Method
+
+nama = []
+
+def print_nama():
+    print("================")
+    for data in nama:
+        print(data)
+
+nama.append('andi')
+print_nama()
+
+nama.append('setiawan')
+print_nama()
+
+nama.append('achmad')
+print_nama()
