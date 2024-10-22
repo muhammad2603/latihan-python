7bed3bd4b23cf0ef45b41a7f513e960cbb33b49f
muhammad2603 <fattahillahmuhammad48@gmail.com>
2024-10-22 09:53:46 +0700

    If Statement

---
+++ if/if.py
@@ -0,0 +1,13 @@
+# Belajar if-statement
+
+orang = 1000
+zombie = 100
+
+if orang < zombie:
+    print("Lari, dunia mau kiamat!")
+
+if orang > zombie:
+    print("Mari basmi zombie!")
+
+if orang == zombie:
+    print("Mari bersiap berperang!")
