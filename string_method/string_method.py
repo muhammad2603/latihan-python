27517baa2d741f3e5074a2848d22a38afd04941a
muhammad2603 <fattahillahmuhammad48@gmail.com>
2024-10-22 12:44:39 +0700

    String Method

---
+++ string_method/string_method.py
@@ -0,0 +1,9 @@
+# Belajar String Methods
+# https://docs.python.org/3.7/library/stdtypes.html#string-methods
+
+nama = "Abdul Rahman Sayuti"
+print(nama)
+print(nama.upper())
+print(nama.capitalize())
+print(nama.title())
+print(nama.split(" "))
